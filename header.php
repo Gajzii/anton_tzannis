34eb9f6229dd0f01b959296255a61fbb8335ce81
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-08-09 11:05:49 +0200

    viewport

--- header.php
+++ header.php
@@ -5,9 +5,12 @@
 <head>
     <?php wp_head(); ?>
     <!-- Links to stylesheet named "style" -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=1.0">
+
     
 </head>
-
+<body>
+    <div class="header">
 <!--- LOGO --->
 <a href="<?php echo get_stylesheet_directory_uri(); ?>index.php">
     <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/image/antontzannis-logo-red.png" />
@@ -55,4 +58,5 @@
         </div>
     </ul>
 </nav>
+</div>
 <script src="https://code.iconify.design/iconify-icon/1.0.1/iconify-icon.min.js"></script>
