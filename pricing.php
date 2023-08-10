cd778b89ba64fc04f452fef1e473bd845d20cea5
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-08-10 14:04:54 +0200

    Påbegyndt pris side

--- pricing.php
+++ pricing.php
@@ -1,1 +1,209 @@
-<html>Priser</html>
+<html>
+<h1>Prisliste</h1>
+<p>Priserne er opgivet i DKK.</p>
+
+<div class="price-grid">
+    <!--- HAIRCUT --->
+    <div class="price-grid-item haircut">
+        <div class="icon-header">
+            <img class="icon-header-icon" src="/assets/images/antontzannis-logo-red.png" alt="haircut icon">
+
+
+            <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>../assets/images/haircut.svg" />
+
+
+
+            <h2>Klip</h2>
+        </div>
+        <div class="secondary-whitespace-horizontal"></div>
+        <div class="price-table">
+            <table>
+                <tbody>
+                    <tr class="performance tableRowFlex">
+                        <td>Dame</td>
+                        <td>Herre<br><br></td>
+                        <td>Barn 0-10 år</td>
+                        <td>Barn 11-14 år<br><br></td>
+                        <td>Studierabat på klip</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                    <tr class="prices tableRowFlex">
+                        <td>675,-</td>
+                        <td>620,-<br><br></td>
+                        <td>365,-</td>
+                        <td>500,-<br><br></td>
+                        <td>-10%</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--- BROWS --->
+    <div class="price-grid-item brows">
+        <div class="icon-header">
+            <img class="icon-header-icon" src="../img/bryn_og_vipper.svg" alt="brows and lashes icon">
+            <h2>Bryn og vipper</h2>
+        </div>
+        <div class="secondary-whitespace-horizontal"></div>
+        <div class="price-table">
+            <table>
+                <tbody>
+                    <tr class="performance tableRowFlex">
+                        <td>Bryn</td>
+                        <td>Vipper</td>
+                        <td>Sammen (bryn og vipper)<br><br></td>
+                        <td>Retning af bryn</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                    <tr class="prices tableRowFlex">
+                        <td>145,-</td>
+                        <td>190,-</td>
+                        <td>290,-<br><br></td>
+                        <td>fra 105,-</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--- PERMANENT --->
+    <div class="price-grid-item permanent">
+        <div class="icon-header">
+            <img class="icon-header-icon" src="../img/permanent.svg" alt="permanent curls icon">
+            <h2>Permanent</h2>
+        </div>
+        <div class="secondary-whitespace-horizontal"></div>
+        <div class="price-table">
+            <table>
+                <tbody>
+                    <tr class="performance tableRowFlex">
+                        <td>Solo<br><br></td>
+                        <td>Perm. inkl. klip og føn</td>
+                        <td>Perm. inkl. klip og føn - langt hår</td>
+                        <td>Perm. pr. spole<br><br></td>
+                        <td>Spiral</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                    <tr class="prices tableRowFlex">
+                        <td>1370,-<br><br></td>
+                        <td>2045,-</td>
+                        <td>2115,-</td>
+                        <td>95,-<br><br></td>
+                        <td>3090,-</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--- OTHER TREATMENTS --->
+    <div class="price-grid-item other-treatments">
+        <div class="icon-header">
+            <img class="icon-header-icon" src="../img/anden_behandling.svg" alt="permanent curls icon">
+            <h2>Anden behandling</h2>
+        </div>
+        <div class="secondary-whitespace-horizontal"></div>
+        <div class="price-table">
+            <table>
+                <tbody>
+                    <tr class="performance tableRowFlex">
+                        <td>Alm. vask<br><br></td>
+                        <td>Opsætning - langt hår</td>
+                        <td>Opsætning - kort hår<br><br></td>
+                        <td>Olaplex (stand alone)</td>
+                        <td>Olaplex (ved klip)</td>
+                        <td>Olaplex (ved farve)<br><br></td>
+                        <td>Kur</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table>
+                <tbody>
+                    <tr class="prices tableRowFlex">
+                        <td>95,-<br><br></td>
+                        <td>fra 1365,-</td>
+                        <td>fra 855,-<br><br></td>
+                        <td>600,-</td>
+                        <td>490,-</td>
+                        <td>365,-<br><br></td>
+                        <td>155,-</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--- COLORING --->
+    <div class="price-grid-item price-grid-item_haircolor haircolor">
+        <div class="icon-header">
+            <img class="icon-header-icon" src="../img/farve.svg" alt="permanent curls icon">
+            <h2>Farve</h2>
+        </div>
+        <div class="secondary-whitespace-horizontal"></div>
+        <div class="price-table price-table_color-column">
+            <div class="price-table_color">
+                <table>
+                    <tbody>
+                        <tr class="performance tableRowFlex">
+                            <td>Dame afrens 1 pk. i forb. m. farve</td>
+                            <td>Dame afrens 2 pk. i forb. m. dame farve - <br>langt/tykt hår<br><br></td>
+                            <td>Dame farve / skyllefarve</td>
+                            <td>Tillæg dame farve + reflex pr. stk.</td>
+                            <td>Dame farve tillæg - langt hår<br><br></td>
+                            <td>Herre farve</td>
+                            <td>Herre reflex m. hætte<br><br></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr class="prices tableRowFlex">
+                            <td>430,-</td>
+                            <td>810,-<br><br><br></td>
+                            <td>360,-</td>
+                            <td>120,-</td>
+                            <td>165,-<br><br></td>
+                            <td>475,-</td>
+                            <td>565,-</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="price-table-gap_color"></div>
+            <div class="price-table_color">
+                <table>
+                    <tbody>
+                        <tr class="performance tableRowFlex">
+                            <td>Dame reflex m. staniol - kort hår</td>
+                            <td>Dame reflex m. staniol - mellemlangt hår</td>
+                            <td>Dame reflex m. staniol - langt hår</td>
+                            <td>Dame reflex m. hætte</td>
+                            <td>Dame reflex m. top</td>
+                            <td>Dame reflexer pr. stk.</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr class="prices tableRowFlex">
+                            <td>840,-</td>
+                            <td>965,-</td>
+                            <td>1120,-</td>
+                            <td>685,-</td>
+                            <td>595,-</td>
+                            <td>155,-</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+</html>
