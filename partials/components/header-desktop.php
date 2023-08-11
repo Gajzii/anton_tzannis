2180e7f8c0ad96f6c5a8e03c503d35200fcbe8a0
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-08-11 13:54:08 +0200

    new header

---
+++ partials/components/header-desktop.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="container">
+        <div class="header-inner">
+        <?php wp_nav_menu(
+    array(
+        'theme_location' => 'top-menu',
+        'container_class' => 'top-menu'
+    )
+    );
+?>
+</div></div>
+
+</header>
