343a3e5346e7537ce34cdf897256fdae9cad9183
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-08-15 14:00:42 +0200

    nav

--- partials/components/header-desktop.php
+++ partials/components/header-desktop.php
@@ -1,28 +1,24 @@
 <header class="header">
-    <div class="header-container">
-        <div class="header-inner">
-
-            <div class="logo-container">
-                <a src="/">
-                    <img class="logo"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/antontzannis-logo-red.png" />
-                </a>
-            </div>
-
-            <div id="menu" onclick="onClickMenu()">
+    <div class="header-inner">
+        <a src="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <img class="logo"
+            src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/antontzannis-logo-red.png" />
+        </a>
+        
+        <div class="menu-container">
+            <div id="dropdownmenu" onclick="onClickMenu()">
                 <div id="bar1" class="bar"></div>
                 <div id="bar2" class="bar"></div>
                 <div id="bar3" class="bar"></div>
             </div>
 
-        <?php wp_nav_menu(
-            array(
-                'theme_location' => 'top-menu',
-                'container_class' => 'top-menu',
-                'menu_class' => 'nav_list'
-            )
-            );
-        ?>
+            <?php wp_nav_menu(
+                array(
+                    'theme_location' => 'top-menu',
+                    'container_class' => 'top-menu',
+                )
+                );
+            ?>
         </div>
     </div>
 </header>
