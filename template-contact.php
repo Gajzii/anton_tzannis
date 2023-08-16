49a5f96ba21b21684893d7a4c046646c596669e8
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-08-16 10:19:08 +0200

    dfgh

--- template-contact.php
+++ template-contact.php
@@ -6,17 +6,16 @@
 <h1>PRiser</h1>
 <h3>sub titiel</h3>
 
-<?php if ( have_rows('pricet') ) : ?>
+<?php if ( have_rows('prices_cut') ) : ?>
 <div class="price-table">
-    <?php while ( have_rows('repeater_field') ) : the_row(); ?>
+    <?php while ( have_rows('prices_cut') ) : the_row(); ?>
     <?php
                 $product_name = get_sub_field('product_name');
-                $product_price = get_sub_field('product_name');
+                $product_price = get_sub_field('product_price');
             ?>
 
     <div class="price-table__item">
-        <span><?= $product_name; ?></span>
-        <span><?= $product_price; ?></span>
+        <span><strong><?= $product_name; ?></strong><?= $product_price; ?>,-</span>
     </div>
     <?php endwhile; ?>
 </div>
