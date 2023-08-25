1b7cec0c743ecd599b07f3eabab44f00439a3b6b
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-08-25 19:24:48 +0200

    fedt

--- template-home.php
+++ template-home.php
@@ -27,78 +27,43 @@
     </div>
 </div>
 <!-- CTA SECTION -->
-<div class="cta-btn-section-container">
-    <div class="page-margin">
-        <div class="cta-section">
-            <!--- FACEBOOK CTA --->
-            <div class="cta_card_inner"
-                style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/cta-facebook.jpg)">
-                <div class="cta_card_inner_background">
-                    <h3 class="goldtext">Følg os på Facebook</h3>
-                    <div class="ctadetails">
-                        <p class="cta_description">Følg os på vores Facebook-side for nyheder mv.</p>
-                        <form action="https://www.facebook.com/AntonTzannis" target="_blank">
-                            <button class="primary-btn center">
-                                <span class="button-leftpart-primary">
-                                    Gå til Facebook
-                                </span>
-                                <span class="button-rightpart-primary">
-                                    <iconify-icon class="button-content-right"
-                                        icon="material-symbols:arrow-right-alt-rounded"></iconify-icon>
-                                </span>
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+<div class="page-margin">
 
-            <div class="primary-whitespace-horizontal"></div>
-            <!--- PRODUCTS CTA --->
-            <div class="cta_card_inner"
-                style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/products.webp)">
-                <div class="cta_card_inner_background">
-                    <h3 class="goldtext">Se produkter</h3>
-                    <div class="ctadetails">
-                        <p class="cta_description">Vi forhandler en række eksklusive produkter i butikken.</p>
-                        <?php echo '<form action="' . get_site_url() . '/products">' ?>
+    <?php if ( have_rows('ctahome') ) : ?>
+    <div class="products_card">
+        <?php while ( have_rows('ctahome') ) : the_row(); ?>
+
+        <?php
+            $cta_title = get_sub_field('cta_title');
+            $cta_link = get_sub_field('cta_link');
+            $cta_background_image = get_sub_field('cta_background_image');
+            $cta_description = get_sub_field('cta_description');
+        ?>
+            
+        <!--- FACEBOOK CTA --->
+        <div class="cta_card_inner product_card_inner" style="background-image: url(<?= $cta_background_image['url']; ?>)">
+            <div class="cta_card_inner_background">
+                <h3 class="goldtext"><?= $cta_title; ?></h3>
+                <div class="ctadetails">
+                    <p class="cta_description"><?= $cta_description; ?></p>
+                    <form target="_blank" action="<?= $cta_link['url']; ?>">
                         <button class="primary-btn center">
                             <span class="button-leftpart-primary">
-                                Gå til produkter
+                                123
                             </span>
                             <span class="button-rightpart-primary">
                                 <iconify-icon class="button-content-right"
                                     icon="material-symbols:arrow-right-alt-rounded"></iconify-icon>
                             </span>
                         </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            <div class="primary-whitespace-horizontal"></div>
-            <!--- OFFER CTA --->
-            <div class="cta_card_inner"
-                style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/scissor.webp)">
-                <div class="cta_card_inner_background">
-                    <h3 class="goldtext">Vi giver studierabat</h3>
-                    <div class="ctadetails">
-                        <p class="cta_description">Har du studiekort, trækker vi 10% fra prisen på klip.</p>
-                        <?php echo '<form action="' . get_site_url() . '/prices">' ?>
-                        <button class="primary-btn center">
-                            <span class="button-leftpart-primary">
-                                Gå til priser
-                            </span>
-                            <span class="button-rightpart-primary">
-                                <iconify-icon class="button-content-right"
-                                    icon="material-symbols:arrow-right-alt-rounded"></iconify-icon>
-                            </span>
-                        </button>
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
+        <?php endwhile; ?>
     </div>
+    
+    <?php endif; ?>
 </div>
 <!----------- OPENING HOURS SECTION ----------->
 <div class="opening-hours-section page-margin" id="opening-hours-section">
