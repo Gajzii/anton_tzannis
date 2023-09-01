948bd6fa2c8b32d9f742651048be2f7c69d285ea
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-09-01 13:24:17 +0200

    dsfghj

--- partials/components/parts/parts-footer-menu.php
+++ partials/components/parts/parts-footer-menu.php
@@ -1,24 +1,30 @@
 <footer>
     <div class="footer-background">
-        <div class="footer-grid">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                <img class="footer-logo"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/antontzannis-logo.svg" />
-            </a>
+            <div class="footer-grid">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <img class="footer-logo"
+                        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/antontzannis-logo.svg" />
+                </a>
 
-            <?php wp_nav_menu(
-                array(
-                    'theme_location' => 'footer-menu',
-                    'container_class' => 'footer-menu'
-                )
-                );
-            ?>
-            <div class="footer-copyright">
-                <h4>©<?php echo get_the_date('Y'); ?> Frisør Anton Tzannis</h4>
+                <?php wp_nav_menu(
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'container_class' => 'footer-menu'
+                    )
+                    );
+                ?>
+                <div class="footer-copyright">
+                    <h4>©<?php echo get_the_date('Y'); ?> Frisør Anton Tzannis</h4>
+                </div>
+            </div>
+            <div>
+            <p class="footer-credit">m.wennerwald og c.nørgaard.</p>
+            <p class="footer-credit_secondary">Credits:
+                <a href="icons8.com" action="_blank">Icons by Icons8</a>
+                <a href="freepik.com" action="_blank">Stockphotos by Freepik</a>
+            </p>
             </div>
         </div>
-        <p class="footer-credit">m.wennerwald og c.nørgaard.</p>
-    </div>
     </div>
 </footer>
 
