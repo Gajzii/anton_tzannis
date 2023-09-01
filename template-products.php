66d6eea5d93c7c44fe86147671c68f74601fd91f
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-09-01 12:53:43 +0200

    stadig ikke sejt

--- template-products.php
+++ template-products.php
@@ -15,12 +15,13 @@
         <?php while ( have_rows('products') ) : the_row(); ?>
 
         <?php
-        $product_name = get_sub_field('product_name');
-        $btn_text = get_sub_field('btn_text');
-        $background_image = get_sub_field('background_image');
-        $product_description = get_sub_field('product_description');
+            $product_name = get_sub_field('product_name');
+            $btn_text = get_sub_field('btn_text');
+            $background_image = get_sub_field('background_image');
+            $product_description = get_sub_field('product_description');
+            $images = get_sub_field('product_gallery');
         ?>
-        
+
 
 
         <div class="cta_card_inner product_card_inner" style="background-image: url(<?= $background_image['url']; ?>)">
@@ -40,26 +41,27 @@
             </div>
         </div>
 
-        <div class="modal" id="productsPopupModal">
-        <div class="products-popup" id="productsPopup">
-            <div class="products-popup-heading">
-                <button type="button" class="products-close-popup goldtext" onclick="closeForm()">&#10006;</button>
-                <h3 class="goldtext center"><?= $product_name; ?></h3>
-            </div>
-
-            <div class="products-slideshow">
-                
-                    <?php foreach( $images as $image ): ?>
+                <?php if($images): ?>
+                    <div class="modal" id="productsPopupModal">
+                        <div class="products-popup" id="productsPopup">
+                            <div class="products-popup-heading">
+                                <button type="button" class="products-close-popup goldtext" onclick="closeForm()">&#10006;</button>
+                                <h3 class="goldtext center"><?= $product_name; ?></h3>
+                            </div>
+                            <div class="products-slideshow">
+                    
+                            <?php foreach( $images as $image ): ?>
                                 <img class="productsSlideImg" src="<?php echo $image['url']; ?>" />
-                    <?php endforeach; ?>
-
-                <button class="slideshow-arrow-left goldtext" onclick="plusDivs(-1)">&#10094;</button>
-                <button class="slideshow-arrow-right goldtext" onclick="plusDivs(1)">&#10095;</button>
-            </div>
-        </div>
-    </div>
-    
-
+                            <?php endforeach; ?>
+                            
+                            <button class="slideshow-arrow-left goldtext" onclick="plusDivs(-1)">&#10094;</button>
+                            <button class="slideshow-arrow-right goldtext" onclick="plusDivs(1)">&#10095;</button>
+                            </div>
+                        </div>
+                    </div>
+                    <?php wp_reset_postdata();
+                endif; ?>
+                    
         <?php endwhile; ?>
     </div>
 
